f1cc0e33ebeaf16d9e226cec6a2e7ffea081c2fa
wsjh0720-lab <wsjh0720@gmail.com>
2026-06-29 15:01:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio',
   assetPrefix: '/portfolio',
  /portfolio/  images: {
     unoptimized: true,
